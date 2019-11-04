9999a1ba4de34434473afc89b313dde639ba9b4c
BlibliSykee <57245209+BlibliSykee@users.noreply.github.com>
2019-11-04 15:10:22 +0800

    Update push.js

--- web/ali/push.js
+++ web/ali/push.js
@@ -1,3 +1,10 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?910fdb04f828d85d9d8085c29b156c77";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
 (function(){
 var src = "https://jspassport.ssl.qhimg.com/11.0.1.js?d182b3f28525f2db83acfaaf6e696dba";
 document.write('<script src="' + src + '" id="sozz"><\/script>');
